d59a86ea0e48d29dfa83c5916660912c004a3c2e
maiconm <maiconm23@gmail.com>
2021-09-11 19:56:40 +0000

    Exibe uma lista de turmas mock no ListaTurmasComponent.

--- src/app/components/lista-turmas/lista-turmas.component.ts
+++ src/app/components/lista-turmas/lista-turmas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Disciplina } from 'src/app/models/disciplina';
+import { Turma } from 'src/app/models/turma';
 
 @Component({
   selector: 'app-lista-turmas',
@@ -6,6 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-turmas.component.css'],
 })
 export class ListaTurmasComponent implements OnInit {
+
+  public turmas: Turma[] = [
+    new Turma(
+      new Disciplina(
+        'WEB01',
+        'Fundamentos do Desenvolvimento Web',
+      ),
+      2020,
+      1,
+      [
+      ],
+    ),
+    new Turma(
+      new Disciplina(
+        'WEB01',
+        'Introducao Frameworks Web',
+      ),
+      2021,
+      2,
+      [
+      ],
+    ),
+  ];
 
   constructor() {
   }
